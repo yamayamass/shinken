65965ad08f59a93935d5772175c5bec182dbcf5f
yamayama <so81y5511ta@gmail.com>
2023-12-22 17:49:09 +0900

    add_sample_code_for_drv8835

---
+++ AE-DRV8835_DEMO/AE-DRV8835_DEMO/DRV8835_STEP_SAMPLE/DRV8835_STEP_SAMPLE.ino
@@ -0,0 +1,36 @@
+int APHASE  = 4;
+int AENBL   = 5;
+int BPHASE  = 6;
+int BENBL   = 7;
+int VR_PIN  = A0;
+unsigned long VR_VALUE = 0;
+
+void setup() 
+{
+  pinMode(APHASE,OUTPUT);
+  pinMode(AENBL,OUTPUT);
+  pinMode(BPHASE,OUTPUT);
+  pinMode(BENBL,OUTPUT);
+  digitalWrite(AENBL,HIGH);
+  digitalWrite(BENBL,HIGH);
+}
+void READ_VR(void)
+{
+  VR_VALUE = analogRead(VR_PIN);
+}
+void DELAY_WAIT(void)
+{
+  for (int i = 0; i < (VR_VALUE /10 + 7) ; i++) delayMicroseconds(100);
+}
+void loop() 
+{
+  READ_VR();
+  digitalWrite(APHASE,HIGH);
+  DELAY_WAIT();
+  digitalWrite(BPHASE,HIGH);
+  DELAY_WAIT();
+  digitalWrite(APHASE,LOW);
+  DELAY_WAIT();
+  digitalWrite(BPHASE,LOW);
+  DELAY_WAIT();
+}
---
+++ AE-DRV8835_DEMO/read_me.txt
@@ -0,0 +1,16 @@
+このフォルダの内容は下記のとおりです。
+
+├─AE-DRV8835_DEMO
+│  read_me.txt: このファイルです。
+│  LICENSE.txt: LICENSEァイルです。
+│  
+└─AE-DRV8835_DEMO: AE-DRV8835のデモスケッチ(Arduino用)
+    ├─DRV8835_DC_SAMPLE: DCモーター用デモ
+    │      DRV8835_DC_SAMPLE.ino inoファイル
+    │      
+    └─DRV8835_STEP_SAMPLE: ステッピングモーター用デモ
+            DRV8835_STEP_SAMPLE.ino: inoファイル
+
+株式会社秋月電子通商
+http://www.akizukidenshi.com/
+2023年6月15日
