ead98d57dfcf9a385c945165261a2ea984d20489
yamayama <so81y5511ta@gmail.com>
2024-01-02 01:09:59 +0900

    add_reflectsensor_notfill

---
+++ shinken_pra/reflectsensor/reflectsensor.ino
@@ -0,0 +1,26 @@
+/**
+   Hardware Connections:
+    Arduino | RPR-220
+    -------------------
+      5V    |   VCC
+      GND   |   MODE
+      5     |   AIN1
+      6     |   AIN2
+      7     |   BIN1
+      8     |   BIN2
+      motor |   AOUT1
+      motor |   AOUT2
+      motor |   BOUT1
+      motor |   BOUT2
+      GND   |   GND
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
